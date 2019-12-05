ff8dee40ef3b2d2f862faada46cdd2e6128c3aa3
Eugenii <glazov.it@gmail.com>
2019-12-05 20:49:14 +0400

    app APPID, fix endpoint for celsius

--- api/endpoints.ts
+++ api/endpoints.ts
@@ -1,3 +1,3 @@
-import { APIKEY } from '@/constants/app';
+import { APPID } from '@/constants/app';
 
-export const GET_WEATHER_BY_CITY_NAME = (cityName: string) => `api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=${APIKEY}`;
+export const GET_WEATHER_BY_CITY_NAME = (cityName: string) => `api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=${APPID}&units=metric`;
--- src/constants/app.ts
+++ src/constants/app.ts
@@ -1,1 +1,1 @@
-export const APIKEY = '';
+export const APPID = '1139c17cf1905f5c00d7ed6c001fbc09';
