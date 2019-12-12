fa55899cc22a540a4610b44e1fa5f7122c033219
Eugenii <glazov.it@gmail.com>
2019-12-12 09:02:54 +0400

    Add Table component

---
+++ src/components/table/table.tsx
@@ -0,0 +1,17 @@
+import React, { FunctionComponent } from 'react';
+import { Table as AntTable } from 'antd';
+import 'antd/lib/table/style/css';
+
+import styles from './table.scss';
+
+export interface ItableProps {
+    dataSource: any;
+    columns: any;
+}
+
+export const Table: FunctionComponent<ItableProps> = ({
+    dataSource,
+    columns,
+}) => {
+    return <AntTable dataSource={dataSource} columns={columns} />;
+}
