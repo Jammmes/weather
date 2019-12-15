ad06b19e1ab38997264eb830e5e01def00b95374
Eugenii <glazov.it@gmail.com>
2019-12-15 22:45:16 +0400

    Use custom hook useTable for home page

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,88 +1,37 @@
+
 import React, { FunctionComponent } from 'react';
 
 import styles from './home.scss';
 import { Table } from '@/components/table';
-// import { useTable } from '../selector';
-// import { useDispatch } from 'react-redux';
-import { Button } from '@/components/button';
+import { useTable } from '../selector';
+import { useDispatch } from 'react-redux';
 
-// export const mockAllTableData = [
-//   {
-//     key: '1',
-//     city: 'Moscow',
-//     temperature: 32,
-//   },
-//   {
-//     key: '2',
-//     city: 'Minsk',
-//     temperature: 42,
-//   },
-//   {
-//     key: '3',
-//     city: 'Samara',
-//     temperature: 21,
-//   },
-// ]
-
-
+export const mockAllTableData = [
+  {
+    key: '1',
+    city: 'Moscow',
+    temperature: 32,
+  },
+  {
+    key: '2',
+    city: 'Minsk',
+    temperature: 42,
+  },
+  {
+    key: '3',
+    city: 'Samara',
+    temperature: 21,
+  },
+]
 
 export const Home: FunctionComponent<{}> = () => {
 
-  // const dispatch = useDispatch();
-  // const { dataSource, columns } = useTable(dispatch);
-
-  const dataSource = [
-    {
-      key: '1',
-      city: 'Moscow',
-      temperature: 32,
-    },
-    {
-      key: '2',
-      city: 'Minsk',
-      temperature: 42,
-    },
-    {
-      key: '3',
-      city: 'Samara',
-      temperature: 21,
-    },
-  ]
-
-  const columns = [
-    {
-      title: 'City',
-      dataIndex: 'city',
-      key: 'city',
-      render: (text: string, record: any) => <Button type='link' onClick={() => console.log((record.city))}>{record.city}</Button>,
-
-    },
-    {
-      title: 'Temperature',
-      dataIndex: 'temperature',
-      key: 'temperature',
-    },
-    {
-      title: 'Up',
-      dataIndex: '',
-      key: 'up',
-      render: (text: string, record: any) => <Button icon='caret-up' onClick={() => console.log('UP', record.key)} />,
-    },
-    {
-      title: 'Down',
-      dataIndex: '',
-      key: 'down',
-      render: (text: string, record: any) => <Button icon='caret-down' onClick={() => console.log('DOWN', record.key)} />,
-    },
-    {
-      title: 'Action',
-      dataIndex: '',
-      key: 'action',
-      render: (text: string, record: any) => <Button icon={+record.key % 2 > 0 ? 'rest' : 'select'} onClick={() => console.log(+record.key % 2 > 0 ? 'Remove' : 'Recover', record.key)} />,
-    },
-  ];
+  const dispatch = useDispatch();
+  const { dataSource, columns } = useTable(mockAllTableData, dispatch);
 
   return <div className={styles.root}>
-    <Table dataSource={dataSource} columns={columns} />
+    <Table columns={columns} dataSource={dataSource} />
   </div>;
+
+
 };
